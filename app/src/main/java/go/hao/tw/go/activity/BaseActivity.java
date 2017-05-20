e21bd89a64c110ae473dd3ce41dbfe4b63843b30
DESKTOP-M33P1NB\Hao <chihhao81@gmail.com>
2017-05-20 16:52:52 +0800

    commit test

---
+++ app/src/main/java/go/hao/tw/go/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package go.hao.tw.go.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hao on 2017/5/20.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
